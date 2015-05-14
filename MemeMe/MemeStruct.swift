d59cba12f3639fe383d9c0b8760e226e8b5687bf
ebuige <ebuige@att.com>
2015-05-14 15:54:49 -0600

    Committed by Udacity

---
+++ MemeMe/MemeStruct.swift
@@ -0,0 +1,15 @@
+//
+//  MemeStruct.swift
+//  MemeMe
+//
+//  Created by Troutslayer33 on 5/10/15.
+//  Copyright (c) 2015 Troutslayer33. All rights reserved.
+//
+// Struct to hold memes data
+
+import UIKit
+struct Meme {
+    var textFieldTop: String?
+    var textFieldBottom: String?
+    var image: UIImage
+    var memeImage: UIImage}
